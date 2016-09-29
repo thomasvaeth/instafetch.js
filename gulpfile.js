755677c212815d832238dc86249f5eaa8f8c64ac
bhague1281 <bhague1281@gmail.com>
2016-09-28 19:17:20 -0700

    added browserify and es6promise polyfill, cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var eslint = require('gulp-eslint');
 var rename = require('gulp-rename');
 var strip = require('gulp-strip-comments');
 var uglify = require('gulp-uglify');
+var browserify = require('gulp-browserify');
 
 gulp.task('lint', function() {
   return gulp.src('./src/**/*.js')
@@ -14,27 +14,24 @@ gulp.task('lint', function() {
   .pipe(eslint.failAfterError());
 });
 
-gulp.task('concatScripts', function() {
-  return gulp.src([
-    'node_modules/fetch-jsonp/build/fetch-jsonp.js',
-    'src/instafetch.js'
-  ])
-  .pipe(concat('instafetch.js'))
-  .pipe(strip())
-  .pipe(gulp.dest('./dist'));
+gulp.task('bundleScripts', function() {
+  return gulp.src('./src/instafetch.js')
+    .pipe(browserify())
+    .pipe(strip())
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('minifyScripts', ['concatScripts'], function() {
+gulp.task('minifyBundle', ['bundleScripts'], function() {
   return gulp.src('./dist/instafetch.js')
   .pipe(uglify())
   .pipe(rename('instafetch.min.js'))
   .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build', ['lint', 'minifyScripts']);
+gulp.task('build', ['lint', 'minifyBundle']);
 
 gulp.task('watch', function() {
-  gulp.watch('./src/**/*.js', ['lint', 'minifyScripts']);
+  gulp.watch('./src/**/*.js', ['lint', 'minifyBundle']);
 });
 
 gulp.task('default', ['build', 'watch']);
